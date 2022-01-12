bd3cb4976afdaa64863ea958ad5c663db57d7813
Oskar <o_swiderski@o2.pl>
2022-01-12 11:05:36 +0100

    zadanie

--- app/src/main/java/com/example/a12012022mobilne/MainActivity.kt
+++ app/src/main/java/com/example/a12012022mobilne/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.a12012022mobilne
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var liczba = 1
+        findViewById<Button>(R.id.pomniejszenieobrazka).setOnClickListener {
+            liczba = liczba-1
+        }
+        findViewById<Button>(R.id.powiekszenieobrazka).setOnClickListener {
+            liczba = liczba+1
+        }
+        when(liczba){
+            1 -> findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pika5)
+            2 -> findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.umbreon)
+            3 -> findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.lapras)
+            4 -> findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.home130gyarados_shiny)
+
+        }
+
+
     }
 }
