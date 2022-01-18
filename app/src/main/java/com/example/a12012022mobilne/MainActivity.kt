7041c4fcf863206b1a398ad1679fe77104c2308a
Oskar <o_swiderski@o2.pl>
2022-01-18 17:34:31 +0100

    test z domu

--- app/src/main/java/com/example/a12012022mobilne/MainActivity.kt
+++ app/src/main/java/com/example/a12012022mobilne/MainActivity.kt
@@ -11,17 +11,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var liczba = 1
         findViewById<Button>(R.id.pomniejszenieobrazka).setOnClickListener {
-            liczba = liczba-1
+            liczba -=1
         }
         findViewById<Button>(R.id.powiekszenieobrazka).setOnClickListener {
-            liczba = liczba+1
+            liczba +=1
         }
         when(liczba){
+            0 -> liczba = 1
             1 -> findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pika5)
             2 -> findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.umbreon)
             3 -> findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.lapras)
             4 -> findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.home130gyarados_shiny)
-
+            5 -> liczba = 5
         }
 
 
